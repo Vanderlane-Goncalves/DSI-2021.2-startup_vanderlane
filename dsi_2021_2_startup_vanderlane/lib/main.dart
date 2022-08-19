ea243dbfc0618b9a436dc0579537ebf562921e50
Vanderlane Gonçalves Cavalcanti Junior <CIANDT\vcavalcanti@lnb035062cps.ciandt.global>
2022-08-19 18:55:48 -0300

    Adição do icone de favorito a lista de scroll infinito

--- dsi_2021_2_startup_vanderlane/lib/main.dart
+++ dsi_2021_2_startup_vanderlane/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) ;
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -28,25 +28,33 @@ class RandomWords extends StatefulWidget {
   @override
   State<RandomWords> createState() => _RandomWordsState();
 }
-class _RandomWordsState extends State<RandomWords>{
+
+class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return ListView.builder(
       padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i){
+      itemBuilder: (context, i) {
         if (i.isOdd) return const Divider();
 
-        final index = i ~/2;
-        if (index >= _suggestions.length){
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10));
         }
+        final alreadySaved = _saved.contains(_suggestions[index]);
         return ListTile(
           title: Text(
             _suggestions[index].asPascalCase,
             style: _biggerFont,
+          ),
+          trailing: Icon(
+            alreadySaved? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved? Colors.red : null,
+            semanticLabel: alreadySaved? 'Remove from saved': 'Save',
           ),
         );
       },
