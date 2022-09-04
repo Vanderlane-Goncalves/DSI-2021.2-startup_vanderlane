57f55cfc5c6427dad120a557845f4af703dce89e
Vanderlane Gonçalves Cavalcanti Junior <CIANDT\vcavalcanti@lnb035062cps.ciandt.global>
2022-09-04 01:46:35 -0300

    Correção do topico-2 com a adicção de lista com duas colunas

--- dsi_2021_2_startup_vanderlane/lib/main.dart
+++ dsi_2021_2_startup_vanderlane/lib/main.dart
@@ -4,21 +4,14 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) ;
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup-Vanderlane Generator',
       theme: ThemeData.dark(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Startup Vanderlane Generator'),
-        ),
-        body: const Center(
-          child: RandomWords(),
-        ),
-      ),
+      home: const RandomWords(),
     );
   }
 }
@@ -28,29 +21,75 @@ class RandomWords extends StatefulWidget {
   @override
   State<RandomWords> createState() => _RandomWordsState();
 }
-class _RandomWordsState extends State<RandomWords>{
+
+class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18);
+  
+  bool _listPair = true;
 
   @override
-  Widget build(BuildContext context){
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i){
-        if (i.isOdd) return const Divider();
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Startup Vanderlane Generator'),
+          leading: IconButton(
+            onPressed: () {
+              setState(() {
+                _listPair ? _listPair = false : _listPair = true;
+              });
+            },
+            icon: Icon(_listPair ? Icons.grid_view : Icons.list),
+          ),
+        ),
+        body: _buildSuggestions());
+  }
 
-        final index = i ~/2;
-        if (index >= _suggestions.length){
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return ListTile(
+  Widget _buildSuggestions() {
+    if (_listPair) {
+      return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return const Divider();
+
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        },
+      );
+    } else {
+      return GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          childAspectRatio: 4,
+          crossAxisCount: 2,
+          crossAxisSpacing: 6.0,
+          mainAxisSpacing: 6.0,
+        ),
+        itemBuilder: (context, i) {
+          if (i >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return Card(child: _buildRow(_suggestions[i]));
+        },
+      );
+    }
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return Dismissible(
+        key: Key(pair.toString()),
+        onDismissed: (direction) {
+          setState(() {
+            _suggestions.remove(pair);
+          });
+        },
+        child: ListTile(
           title: Text(
-            _suggestions[index].asPascalCase,
+            pair.asPascalCase,
             style: _biggerFont,
           ),
-        );
-      },
-    );
+        ));
   }
 }
-
